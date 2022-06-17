57e95d6784cae5377cca1ec07d000d11a031e89f
Vansh Patpatia <68200019+vansh10patpatia@users.noreply.github.com>
2022-06-18 00:18:46 +0530

    app.js changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import logo from './logo.svg'
 import './App.css';
 import {store} from './store';
 import { Provider, connect } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import Navigation from './routes/index'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return(
     <Provider store={store}>
-        {/* <Navigator /> */}
-        <h1>Basic React Redux Template</h1>
+        <BrowserRouter>
+            <Navigation />
+        </BrowserRouter>
     </Provider>
   )
 }
