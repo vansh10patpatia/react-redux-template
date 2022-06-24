dc30b6be01fd5f8f9b762884f1848f90cfcb6018
Vansh Patpatia <68200019+vansh10patpatia@users.noreply.github.com>
2022-06-24 18:33:21 +0530

    basic validation added

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -10,6 +10,13 @@ export const contactFormValidators =
 export const loginValidators = 
         Yup.object({
             email:Yup.string().required("Please enter email!").email("Please enter a valid email"),
+            password:Yup.string().required("Please enter password!"),  
+        })
+    
+export const registerValidators =
+        Yup.object({
+            name:Yup.string().required("Please enter name!").min(2, "Invalid Name"),
+            email:Yup.string().required("Please enter email!").email("Please enter a valid email"),
             password:Yup.string().required("Please enter password!"),
-            
+            confirmPassword:Yup.string().required("Please enter confirm password!").oneOf([Yup.ref('password'), null], "Passwords must match"),
         })
